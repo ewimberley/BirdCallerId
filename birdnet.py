c58dfe4044c7f0bafd9346118741acd6a69156cd
Eric Wimberley <wimberleyeric@gmail.com>
2019-06-20 23:41:36 -0400

    Bird recognition!

--- birdnet.py
+++ birdnet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from birdnet import *
+from birdnetRNN import *
 
 import sys
 from scipy.io.wavfile import read
@@ -86,21 +86,34 @@ def createDataset(dataFile, sampleLenSeconds, samplesPerMinute):
         print("Windows per second: " + str(windowsPerSec))
         numSamples = int(samplesPerMinute * time / 60.0)
         print("Number of samples: " + str(numSamples))
+        windowsPerSample = int(sampleLenSeconds * windowsPerSec)
+        print("Windows per sample: " + str(windowsPerSample))
         #print(x.shape)
         #print(x)
-        sampleStartIndeces = np.linspace(0, numWindows, num=numSamples, dtype=np.int32)
-        print(sampleStartIndeces)
+        sampleStartIndeces = np.linspace(0, numWindows - windowsPerSample, num=numSamples, dtype=np.int32)
+        #print(sampleStartIndeces)
+        xArray = X_train
+        yArray = y_train
+        if dataset == "validate":
+            xArray = X_validate
+            yArray = y_validate
+        elif dataset == "test":
+            xArray = X_test
+            yArray = y_test
         for startIndex in sampleStartIndeces:
-            endIndex = startIndex + int(sampleLenSeconds * numWindows)
+            endIndex = startIndex + windowsPerSample
             sample = x[startIndex:endIndex,]
-            print(sample)
-        y = speciesId
+            xArray.append(sample)
+            yArray.append(speciesId)
         #print(x)
         #if dataset == "train":
 
-    return X_train, y_train, X_validate, y_validate, X_test, y_test
+    return np.stack(X_train), np.stack(y_train), np.stack(X_validate), np.stack(y_validate), np.stack(X_test), np.stack(y_test)
+    #return X_train, y_train, X_validate, y_validate, X_test, y_test
 
-createDataset("data.csv", 5.0, 60)
+X_train, y_train, X_validate, y_validate, X_test, y_test = createDataset("data.csv", 5.0, 60)
+print("*" * 30)
+trainModel(X_train, y_train, X_validate,y_validate)
 exit(0)
 
 #Below is for debugging purposes
--- birdnetRNN.py
+++ birdnetRNN.py
@@ -2,21 +2,44 @@
 import numpy as np
 
 from keras import layers
+from keras.models import Sequential
 from keras.layers import recurrent
 from keras.models import Model
+from sklearn.metrics import confusion_matrix
+from keras.utils.np_utils import to_categorical
 
 # Try replacing GRU, or SimpleRNN.
 RNN = layers.LSTM
 EPOCS = 5
 HIDDEN_SIZE = 128
 BATCH_SIZE = 128
-LAYERS = 1
+LAYERS = 2
 
 def trainModel(X_train, y_train, X_test, y_test):
+    print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+    numSamples = X_train.shape[0] #len(X_train)
+    numWindows = X_train.shape[1]
+    numFreqs = X_train.shape[2]
+    print(np.unique(y_train))
+    numClasses = np.unique(y_train).shape[0]
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    print(y_test)
+    print("Samples: " + str(numSamples) + "\nWindows: " + str(numWindows) + "\nFrequency windows: " + str(numFreqs))
     model = Sequential()
-    for _ in range(LAYERS):
-        model.add(RNN(HIDDEN_SIZE))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
+    model.add(RNN(numFreqs, return_sequences=True))
+    for _ in range(LAYERS-1):
+        model.add(RNN(numFreqs))
+    model.add(layers.Dense(numClasses, activation='sigmoid'))
+    #model.add(layers.TimeDistributed(layers.Dense(len(X_train), activation='softmax')))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print("Training")
     model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=EPOCS, batch_size=64)
+    print(model.summary())
+    print('Evaluation')
+    loss, acc = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE)
+    print("Loss: " + str(loss))
+    print("Accuracy: " + str(acc))
+    y_pred = model.predict(X_test)
+    matrix = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+    print(matrix)
